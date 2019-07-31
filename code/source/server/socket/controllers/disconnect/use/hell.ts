95de7dc186022093f97292ef8b1029e159b1556c
alihejrati <alihejrati1996@gmail.com>
2019-08-01 01:50:39 +0430

    commit

--- code/source/server/socket/controllers/disconnect/use/hell.ts
+++ code/source/server/socket/controllers/disconnect/use/hell.ts
@@ -1,4 +1,5 @@
 async function hell(socket: SocketIO.Socket, event, message, next, options: options) {
+    socket['_'].temporary.watchdog.layer = '/disconnect/**';
     await mongodb.findOneAndUpdate('socket', {socketId: socket['_'].socketId.toString()}, {state: 'disconnect', _: socket['_']});
 }
 
